623b8f2cb4a7174983eb5b319c84d1f323449ea5
James O'Halloran <james.p.ohalloran@gmail.com>
2022-04-08 15:18:10 -0300

    initial commit

---
+++ tina-admin/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  define: {
+    "process.env": {
+      VITE_CLIENT_URL: process.env.VITE_CLIENT_URL,
+    },
+    "process.platform": "win32",
+  },
+  plugins: [react()],
+});
